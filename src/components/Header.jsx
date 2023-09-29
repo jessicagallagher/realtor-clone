02dcc97e4b431d23a3c3a81b9a4a02992d4c8ca8
jessicagallagher <jessicalyngallagher@me.com>
2023-09-29 16:42:23 -0400

    add menu links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function Header() {
   const location = useLocation();
+  const navigate = useNavigate();
 
   const pathMatchRoute = (route) => {
     if (route === location.pathname) {
@@ -17,6 +18,7 @@ export default function Header() {
             src='https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg'
             alt='realtor website logo'
             className='h-5 cursor-pointer'
+            onClick={() => navigate('/')}
           />
         </div>
         <div>
@@ -25,6 +27,7 @@ export default function Header() {
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
                 pathMatchRoute('/') && 'text-black border-b-red-500'
               }`}
+              onClick={() => navigate('/')}
             >
               Home
             </li>
@@ -32,13 +35,16 @@ export default function Header() {
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
                 pathMatchRoute('/offers') && 'text-black border-b-red-500'
               }`}
+              onClick={() => navigate('/offers')}
             >
               Offers
             </li>
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMatchRoute('/sign-incursor-pointer ') && 'text-black border-b-red-500'
+                pathMatchRoute('/sign-incursor-pointer ') &&
+                'text-black border-b-red-500'
               }`}
+              onClick={() => navigate('/sign-in')}
             >
               Sign In
             </li>

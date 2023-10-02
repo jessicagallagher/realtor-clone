6fd7d3599744586d6f95c81865334af2a639ba5e
jessicagallagher <jessicalyngallagher@me.com>
2023-10-02 15:13:18 -0400

    fix conditional in header css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,9 @@ export default function Header() {
 
   const pathMatchRoute = (route) => {
     if (route === location.pathname) {
-      return true
+      return true;
     }
-  }
+  };
 
   return (
     <div className='bg-white border-b shadow-sm sticky top-0 z-50'>
@@ -25,7 +25,7 @@ export default function Header() {
           <ul className='flex space-x-10'>
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMatchRoute('/') && 'text-black border-b-red-500'
+                pathMatchRoute('/') && '!text-black !border-b-red-500'
               }`}
               onClick={() => navigate('/')}
             >
@@ -33,7 +33,7 @@ export default function Header() {
             </li>
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMatchRoute('/offers') && 'text-black border-b-red-500'
+                pathMatchRoute('/offers') && '!text-black !border-b-red-500'
               }`}
               onClick={() => navigate('/offers')}
             >
@@ -41,8 +41,7 @@ export default function Header() {
             </li>
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMatchRoute('/sign-incursor-pointer ') &&
-                'text-black border-b-red-500'
+                pathMatchRoute('/sign-in') && '!text-black !border-b-red-500'
               }`}
               onClick={() => navigate('/sign-in')}
             >

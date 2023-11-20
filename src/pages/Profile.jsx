90182c228961e59c6ffd8127bd4b9bee9eef161b
jessicagallagher <jessicalyngallagher@me.com>
2023-11-20 12:43:08 -0500

    create prviate route for profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,99 @@
+import { useState, useEffect } from 'react';
 import { FcHome } from 'react-icons/fc';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, updateProfile } from 'firebase/auth';
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  updateDoc,
+  where,
+} from 'firebase/firestore';
+import { toast } from 'react-toastify';
+import { db } from '../firebase';
 
 export default function Profile() {
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const [changeDetail, setChangeDetail] = useState(false);
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+  const { name, email } = formData;
+
+  const onLogout = () => {
+    auth.signOut();
+    navigate('/');
+  };
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+        // update name in firestore database
+        const docRef = doc(db, 'users', auth.currentUser.uid);
+        await updateDoc(docRef, {
+          name,
+        });
+      }
+      toast.success('Profile details updated');
+    } catch (error) {
+      toast.error('Could not update profile details');
+    }
+  };
+
+  // useEffect(() => {
+  //   const fetchUserListings = async () => {
+  //     const listingRef = collection(db, 'listings');
+  //     const q = query(
+  //       listingRef,
+  //       where('userRef', '==', auth.currentUser.uid),
+  //       orderBy('timestamp', 'desc')
+  //     );
+  //     const querySnap = await getDocs(q);
+  //     let listings = [];
+  //     querySnap.forEach((doc) => {
+  //       return listings.push({
+  //         id: doc.id,
+  //         data: doc.data(),
+  //       });
+  //     });
+  //     setListings(listings);
+  //     setLoading(false);
+  //   };
+  //   fetchUserListings();
+  // }, [auth.currentUser.uid]);
+
+  const onDelete = async (listingID) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      await deleteDoc(doc(db, 'listings', listingID));
+      const updatedListings = listings.filter(
+        (listing) => listing.id !== listingID
+      );
+      setListings(updatedListings);
+      toast.success('Successfully deleted the listing');
+    }
+  };
+
+  const onEdit = (listingID) => {
+    navigate(`/edit-listing/${listingID}`);
+  };
+
   return (
     <>
       <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
@@ -11,22 +103,27 @@ export default function Profile() {
             {/* name input */}
             <input
               type='text'
+              value={name}
               id='name'
+              disabled
               className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out`}
             />
             {/* email input */}
             <input
-              type='text'
+              type='email'
+              value={email}
               id='email'
               disabled
-              className='mb-6 w-full px-4 py-2 text-xl text-gray 700 bg-white border border-gray-300 rounded transition ease-in-out'
+              className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out'
             />
             <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6'>
               <p className='flex items-center'>
-                Do you want to change your name? &nbsp
-                <span>Apply change</span>
+                Do you want to change your name?
+                <span className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer'>
+                  Edit
+                </span>
               </p>
-              <p className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer'>
+              <p onClick={onLogout} className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer'>
                 Sign out
               </p>
             </div>
